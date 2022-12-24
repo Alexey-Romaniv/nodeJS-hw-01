ece0ff962415ecd8e7b23f17aa358631200cd681
Alexey-Romaniv <105770723+Alexey-Romaniv@users.noreply.github.com>
2022-12-24 17:46:43 +0200

    made all

---
+++ contacts.js
@@ -0,0 +1,49 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+// TODO: задокументувати кожну функцію
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+};
+
+const getContactById = async (contactId) => {
+  const data = await listContacts();
+  const res = data.find(({ id }) => id === contactId);
+  return res;
+};
+
+const removeContact = async (contactId) => {
+  const data = await listContacts();
+  const index = data.findIndex(({ id }) => id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  data.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+  return;
+};
+
+const addContact = async (name, email, phone) => {
+  const data = await listContacts();
+  const id = nanoid();
+  const newContact = {
+    name,
+    email,
+    phone,
+    id,
+  };
+  const [result] = data.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+  return result;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
